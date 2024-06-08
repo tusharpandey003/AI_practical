994081fa66ba700b4f1803a323be010f1d37400a
tusharpandey003 <pandeytushar003@gmail.com>
2024-06-08 20:31:45 +0530

    Html file added

--- py_files/temp.py
+++ py_files/temp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
+import plotly.express as px
+
 
 # Define the start and end date for the DataFrame
 start_date = '2023-01-01'
@@ -22,22 +22,24 @@ def generate_temperature(date):
     day = date.day
     # Define temperature range for each month (example ranges)
     base_temp = {
-        1: 9,   # January
-        2: 13,   # February
+        1: 10,   # January
+        2: 14,   # February
         3: 18,   # March
         4: 23,   # April
         5: 29,   # May
         6: 36,   # June
         7: 43,   # July
-        8: 37,   # August
-        9: 32,   # September
-        10: 29,  # October
-        11: 24,  # November
-        12: 18   # December
+        8: 40,   # August
+        9: 37,   # September
+        10: 34,  # October
+        11: 27,  # November
+        12: 19   # December
     }
     # Generate a random deviation within [-3, 3]
-    deviation = np.random.randint(-2, 2)
-    if month <= 6:
+    deviation = np.random.randint(-3, 3)
+    if month <= 3:
+        r = day*0.1
+    elif month < 7:
         r = day*0.2
     else:
         r = day * (-0.2)
@@ -46,7 +48,7 @@ def generate_temperature(date):
     seasonal_variation = 5 * np.sin(2 * np.pi * (month - 1) / 12)
 
     # Calculate the temperature for the day
-    temperature = base_temp[month] + deviation+seasonal_variation+r
+    temperature = base_temp[month] + seasonal_variation+deviation+r
     return temperature
 
 
@@ -55,7 +57,7 @@ df['Temperature'] = df['Date'].apply(generate_temperature)
 print(df)
 
 # # Display the DataFrame
-df.to_csv("day.csv")
+# df.to_csv("day.csv")
 
 # # Plot the temperature trend
 # plt.figure(figsize=(10, 6))
@@ -65,3 +67,6 @@ df.to_csv("day.csv")
 # plt.title('Temperature Trend (2024)')
 # plt.grid(True)
 # plt.show()
+
+fig = px.line(df, x='Date', y="Temperature")
+fig.write_html("temp.html")
